1d11d4abee0a023d6e2fb334c60c9df5b0e67089
Mittlebron <573844716@qq.com>
2024-12-10 17:05:00 +0800

    Update: HomePage NavBar         1.navbar         2.add UnicaOne font         3.global css change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,17 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import {NzFlexDirective} from 'ng-zorro-antd/flex';
+import {NzAvatarComponent} from 'ng-zorro-antd/avatar';
+import { QuickDown } from './animations/animation';
+import {NzTypographyComponent} from 'ng-zorro-antd/typography';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, NzFlexDirective, NzAvatarComponent, NzTypographyComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [QuickDown]
 })
 export class AppComponent {
   isCollapsed = false;
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {NzButtonModule} from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
+  imports: [ NzButtonModule ],
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
